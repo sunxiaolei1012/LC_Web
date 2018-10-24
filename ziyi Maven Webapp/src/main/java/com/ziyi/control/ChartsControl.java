a578dab30248c0b9b5ce7860ff7e41b796adbaa1
chen <1321712400@qq.com>
2018-10-24 14:15:27 +0800

    123

---
+++ ziyi Maven Webapp/src/main/java/com/ziyi/control/ChartsControl.java
@@ -0,0 +1,14 @@
+package com.ziyi.control;
+
+import com.opensymphony.xwork2.ActionSupport;
+/**
+ * 图表统计
+ * @author 陈玉博
+ * @date 2018年10月24日下午2:12:42
+ */
+public class ChartsControl extends ActionSupport{
+	
+	
+	
+
+}
